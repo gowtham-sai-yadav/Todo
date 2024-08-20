2dace55571e77f8857e76a0fe5f272d7fb4c4f99
gowtham-sai-yadav <gowtham.23bcs10168@sst.scaler.com>
2024-08-20 16:26:32 +0530

    Introduced new fields

--- todos/models.py
+++ todos/models.py
@@ -5,7 +5,14 @@ class Todo(models.Model):
     description = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-
+    rewards_for_completion = models.IntegerField(null=True, blank=True)
+    STATUS_CHOICES = [
+    ('pending', 'Pending'),
+    ('in_progress', 'In Progress'),
+    ('completed', 'Completed'),
+    ('deferred', 'Deferred'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     
 
     def __str__(self):
